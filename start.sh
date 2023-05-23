c043eea3ff14653a283d18faa841e9acbf56c9b3
masloyet <masloyet@gmail.com>
2023-05-23 20:17:08 +0000

    Compatibility

--- start.sh
+++ start.sh
@@ -1,22 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
-if ! command -v nvim &> /dev/null
+root="$(cd "$(dirname $0)" && pwd -P)"
+
+if ! command -v nvim 2>&1 > /dev/null
 then
     echo "nvim could not be found"
     exit
 else
     echo "Setting up nvim config"
     mkdir -p ~/.config/nvim/lua/
-    ln -sf `realpath init.vim` ~/.config/nvim/
-    ln -sf `realpath lsp.lua` ~/.config/nvim/lua/
-    ln -sf `realpath treesitter.lua` ~/.config/nvim/lua/
+    ln -sf $root/init.vim ~/.config/nvim/
+    ln -sf $root/lsp.lua ~/.config/nvim/lua/
+    ln -sf $root/treesitter.lua ~/.config/nvim/lua/
 fi
 
-echo "symlinking in .zshrc"
-ln -sf `realpath .zshrc` ~/.zshrc
-
 echo "Installing vimplug"
-source vimplug_nvim.sh
+. $root/vimplug_nvim.sh
 
 echo "Installing fzf"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
