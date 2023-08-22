714f69ece1efad99611fa5731acca0117a1b52ad
Mason Loyet <masloyet@gmail.com>
2023-08-22 00:36:09 +0000

    Add some more stuff, rework the shell.

--- .zshrc
+++ .zshrc
@@ -4,7 +4,7 @@ setopt extendedglob
 
 # oh-my-zsh stuff.
 
-export ZSH="/Users/masonloyet/.oh-my-zsh"
+export ZSH="<home>/.oh-my-zsh"
 export LANG=en_US.UTF-8
 DISABLE_UPDATE_PROMPT="true"
 plugins=(git)
@@ -15,20 +15,9 @@ source $ZSH/oh-my-zsh.sh
 
 export EDITOR='nvim'
 
-export PATH="/usr/local/opt/llvm/bin:$PATH"
-export PATH="/Users/masonloyet/flutter/flutter/bin:$PATH"
-export PATH="/usr/local/smlnj/bin:$PATH"
-export PATH="/Users/masonloyet/bin/:$PATH"
-
 # aliases
 
-alias smlnj="rlwrap sml"
-alias acl2="rlwrap saved_acl2"
-alias lisp="rlwrap sbcl"
-
 alias vim=nvim
-alias f3=/Users/masonloyet/flutter/flutter3/flutter/bin/flutter
-
 alias gap='git add --patch'
 
 # fzf history
--- start.sh
+++ start.sh
@@ -1,23 +1,86 @@
 #!/bin/sh
 
 root="$(cd "$(dirname $0)" && pwd -P)"
+user=$(whoami)
 
-if ! command -v nvim 2>&1 > /dev/null
-then
-    echo "nvim could not be found"
-    exit
-else
-    echo "Setting up nvim config"
-    mkdir -p ~/.config/nvim/lua/
-    ln -sf $root/init.vim ~/.config/nvim/
-    ln -sf $root/lsp.lua ~/.config/nvim/lua/
-    ln -sf $root/treesitter.lua ~/.config/nvim/lua/
+# Check dependencies.
+errs=0
+if ! command -v git 2>&1 > /dev/null; then
+    echo "git not found"
+    errs=1
+elif ! command -v nvim 2>&1 > /dev/null; then
+    echo "nvim not found"
+    errs=1
+elif ! command -v zsh 2>&1 > /dev/null; then
+    echo "zsh not found"
+    errs=1
+elif ! command -v sed 2>&1 > /dev/null; then
+    echo "sed not found"
+    errs=1
 fi
 
-echo "Installing vimplug"
+if [ $errs -eq 1 ]; then
+    echo "install required packages"
+    exit 1
+fi
+
+echo ""
+echo "##############################"
+echo "zsh"
+echo "##############################"
+echo ""
+
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+cp -v $root/.zshrc ~
+sed -i "s#<home>#$HOME#" ~/.zshrc
+# Copy here as .zshrc tends to have host-specific modifications.
+echo "Changing default shell..."
+chsh -s $(which zsh)
+
+echo ""
+echo "##############################"
+echo "Neovim"
+echo "##############################"
+echo ""
+
+mkdir -pv ~/.config/nvim/lua/
+ln -sfv $root/init.vim ~/.config/nvim/
+ln -sfv $root/lsp.lua ~/.config/nvim/lua/
+ln -sfv $root/treesitter.lua ~/.config/nvim/lua/
+
+echo ""
+echo "##############################"
+echo "vimplug"
+echo "##############################"
+echo ""
+
 . $root/vimplug_nvim.sh
 
-echo "Installing fzf"
+echo ""
+echo "##############################"
+echo "fzf"
+echo "##############################"
+echo ""
+
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+echo ""
+echo "##############################"
+echo "gdb"
+echo "##############################"
+echo ""
+
+ln -sfv $root/.gdbinit ~
+
+echo ""
+echo "##############################"
+echo "tmux"
+echo "##############################"
+echo ""
+
+ln -sfv $root/.tmux.conf ~
+
+echo ""
+echo "Done!"
+echo ""
